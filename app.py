ae0a72500624c0d28b61e229909cfeb9aff5ceab
Radamés Ajna <radames@users.noreply.huggingface.co>
2023-08-21 16:50:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import gradio as gr
 import copy
 import time
@@ -8,8 +9,8 @@ from huggingface_hub import hf_hub_download
 
 llm = Llama(
     model_path=hf_hub_download(
-        repo_id="TheBloke/Llama-2-7B-Chat-GGML",
-        filename="llama-2-7b-chat.ggmlv3.q5_0.bin",
+        repo_id=os.environ.get("REPO_ID", "TheBloke/Llama-2-7B-Chat-GGML"),
+        filename=os.environ.get("MODEL_FILE", "llama-2-7b-chat.ggmlv3.q5_0.bin"),
     ),
     n_ctx=2048,
     n_gpu_layers=50, # change n_gpu_layers if you have more or less VRAM 
